7158292f898ed97f16c14c359f37d390c78152e1
rogeriop <pinheiroxrogerio@gmail.com>
2019-10-26 17:02:24 -0300

    Curso devReactJs #06.12: 02 Consumindo dados de uma API Rest com useState/useEffect e useReducer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import axios from 'axios'
 /*
 axios
@@ -16,10 +16,49 @@ axios
     console.log(res)
   })
 */
+const url = 'https://mymoney-rogerio.firebaseio.com/movimentacoes/2019-08.json'
+
+// função pura
+// recebe um estado + parametros e retorna alguma coisa baseado nos parametros
+// é mais fácil de testar
+
+// manipular meu estado
+const reducer = (state, action) => {
+  if(action.type === 'REQUEST') {
+    return {
+      ...state, // tras tudo dentro de state e substitui só o abaixo
+      loading: true
+    }
+  }
+  if(action.type === 'SUCCESS'){
+    return {
+      ...state,
+      loading: false,
+      data: action.data
+    }
+  }
+  return state
+}
+
 function App() {
+  const [data, dispatch] = useReducer(reducer, {
+    loading: true,
+    data: {}
+  })
+
+  useEffect(() => {
+    dispatch ({ type: 'REQUEST' })
+    axios
+      .get(url)
+      .then(res => {
+       dispatch({ type: 'SUCCESS', data: res.data })
+      })
+  }, [])
   return (
     <div>
       <h1>MyMoney</h1>
+      { JSON.stringify(data) }
+      { data.loading && <p>Loading . . .</p>}
     </div>
   );
 }
