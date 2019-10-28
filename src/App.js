4590c5a118a007349f167de49bae7ef751e07da8
rogeriop <pinheiroxrogerio@gmail.com>
2019-10-28 10:43:52 -0300

    Curso devReactJs #07.04: 03 - Hook personalizado: useDelete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import useGet from './useGet'
 import usePost from './usePost'
+import useDelete from './useDelete'
+
 import axios from 'axios'
 const url = 'https://mymoney-rogerio.firebaseio.com/movimentacoes/2019-08.json'
 
@@ -8,9 +10,14 @@ const url = 'https://mymoney-rogerio.firebaseio.com/movimentacoes/2019-08.json'
 function App() {
   const data = useGet(url)
   const [postData, post] = usePost(url)
+  const [deleteData, remove] = useDelete()
 
   const saveNew = () => {
     post({valor: 10, descricao: 'olá'})
+  }
+
+  const doRemove = () => {
+    remove('https://mymoney-rogerio.firebaseio.com/movimentacoes/2019-08/-LsHM2VXVMUxXYEI4bZi.json')
   }
 
   return (
@@ -20,6 +27,8 @@ function App() {
       { data.loading && <p>Loading . . .</p>}
       <button onClick={saveNew}>Salvar</button>
       <pre>{JSON.stringify(postData)}</pre>
+      <button onClick={doRemove}>Delete</button>
+      <pre>{JSON.stringify(deleteData)}</pre>
     </div>
   );
 }
