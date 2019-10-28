e324ef73569c3736969fcd7f9bb1a4da4bd5ea77
rogeriop <pinheiroxrogerio@gmail.com>
2019-10-28 10:05:48 -0300

    Curso devReactJs #07.03: 02 - Hook personalizado: usePost

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import useGet from './useGet'
-
+import usePost from './usePost'
+import axios from 'axios'
 const url = 'https://mymoney-rogerio.firebaseio.com/movimentacoes/2019-08.json'
 
 
 function App() {
   const data = useGet(url)
+  const [postData, post] = usePost(url)
+
+  const saveNew = () => {
+    post({valor: 10, descricao: 'olá'})
+  }
+
   return (
     <div>
       <h1>MyMoney</h1>
       { JSON.stringify(data) }
       { data.loading && <p>Loading . . .</p>}
+      <button onClick={saveNew}>Salvar</button>
+      <pre>{JSON.stringify(postData)}</pre>
     </div>
   );
 }
