d75660635fa18db565ef4693830cb355e2ad94bc
rogeriop <pinheiroxrogerio@gmail.com>
2019-10-29 11:15:49 -0300

    Curso devReactJs #07.08: 07 - Organizando a navegação com React-Router-Dom

--- src/App.js
+++ src/App.js
@@ -1,35 +1,18 @@
 import React from 'react';
-import Rest from './rest'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from './elements/Header'
-import Meses from './Meses'
-import AdicionarMes from './AdicionarMes'
-
-
-const baseURL = 'https://mymoney-rogerio.firebaseio.com/'
-const { useGet, usePost, useDelete } = Rest(baseURL)
+import Home from './pages/Home'
+import Movimentacoes from './pages/Movimentacoes'
 
 function App() {
-  //const data = useGet('movimentacoes/2019-08')
-  const data = useGet('meses')
-  //const [postData, post] = usePost('movimentacoes/2019-08')
-  //const [deleteData, remove] = useDelete()
-
-  const saveNew = () => {
-    //post({valor: 10, descricao: 'olá'})
-  }
-
-  const doRemove = () => {
-    //remove('movimentacoes/2019-08/-LsImOHv6IHv7N4So3mA')
-  }
-
   return (
-    <div>
-      <Header />
-      <div className='container'>
-        <AdicionarMes />
-        <Meses />
+    <Router>
+      <div>
+        <Header />
+        <Route path='/' exact component={Home} />
+        <Route path='/movimentacoes/:data' component={Movimentacoes} />
       </div>
-    </div>
+    </Router>
   );
 }
 
